4824d694d903ff3701d5b50226254a3a195e3fc7
jeeasistio <jeeasistio08@gmail.com>
2022-02-20 18:17:50 +0800

    .

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -3,6 +3,7 @@ import nookies from 'nookies'
 
 export default handler
 .post((req, res) => {
+  console.log(req, res, 123)
   nookies.destroy({ res }, 'auth-token', { path: '/' })
   nookies.destroy({ res }, 'loggedInUser', { path: '/' })
   res.status(200).json({ msg: 'Logged Out '})
