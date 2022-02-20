273c4d148eac2c77296487538e56f68271ac4f1a
jeeasistio <jeeasistio08@gmail.com>
2022-02-21 01:00:36 +0800

    privacy and terms removed

--- components/HFLayout.js
+++ components/HFLayout.js
@@ -10,7 +10,6 @@ export default function HFLayout({ children }) {
           sticky="bottom"
           className="w-100 p-4 d-flex flex-column-reverse flex-sm-row justify-content-sm-between align-items-center">
           <p className="text-muted">&copy; 2020</p>
-          <p className="text-muted">Privacy and Terms</p>
         </Navbar>
       </HLayout>
     </>
