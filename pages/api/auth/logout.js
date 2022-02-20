163921158b2fbcb29cd47af9157233463abece6a
jeeasistio <jeeasistio08@gmail.com>
2022-02-20 18:37:04 +0800

    cookie destroying on logout fixed

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -2,7 +2,7 @@ import handler from '../../../lib/handler'
 import nookies from 'nookies'
 
 export default handler
-.post((req, res) => {
+.get((req, res) => {
   res.setHeader('Set-Cookie', 'auth-token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT');
   res.setHeader('Set-Cookie', 'loggedInUser=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT');
   res.status(200).json({ msg: 'Logged Out '})
